4503d947720536243ce7d810824d6dc716720574
ryanseaton568 <ryanseaton568@outlook.com>
2016-07-08 15:17:15 -0500

    First program I ever wrote. Simple println commands.

---
+++ program1.java
@@ -0,0 +1,24 @@
+/* NAME:    Joseph Ryan Seaton
+ * CLASS:   CSC 102.002
+ * PROGRAM: program1
+ * DATE:    1/21/2015
+ * 
+ * DESCRIPTION
+ * This program prints out my name, hometown, birthtown, and a pun.
+ * */
+
+
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class program1 {
+  public static void main(String [] args ){
+    System.out.println("My name is Joseph Seaton.");
+    System.out.println("My hometown is Mansfield, Tx.");
+    System.out.println("I was born in Irving, Tx.");
+    System.out.println("Did you hear about the guy whose whole left side was cut off? He's all right now.");
+  }//close class
+}//close main
